bc949ccbd815f5ae8d38275f973eecc13914eb5d
Ivan <=>
2024-11-02 16:38:15 +0300

    Доработка стилей и формы

--- app/add-todo/page.tsx
+++ app/add-todo/page.tsx
@@ -1,15 +1,27 @@
 "use client";
+import { Box, Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
 import { useState } from "react";
 
 const Page: React.FC = () => {
-    const [text, setText] = useState('11');
-  
-    console.log(text)
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+
+    const changeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    };
+    const changeDescription = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDescription(event.target.value);
+    };
     
     return (
-      <div>
-        Form
-      </div>
+      <Box component="form" className="add-todo-form">
+        <TextField onChange={changeName} label="Название" variant="outlined" />
+        <TextField onChange={changeDescription} label="Описание" variant="outlined" />
+        <div className="save-todo-btn">
+            <Button disabled={!name || !description} variant="outlined">Сохранить</Button>
+        </div>
+      </Box>
     );
   }
   
